296fd614f88dbc96e1001abe7e95238b90262781
ankurnak <112162607+ankurnak@users.noreply.github.com>
2023-07-09 10:58:21 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace LinkedListExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.add = new System.Windows.Forms.Button();
+            this.remove = new System.Windows.Forms.Button();
+            this.display = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.clear = new System.Windows.Forms.Button();
+            this.count = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // add
+            // 
+            this.add.Location = new System.Drawing.Point(101, 306);
+            this.add.Name = "add";
+            this.add.Size = new System.Drawing.Size(75, 23);
+            this.add.TabIndex = 0;
+            this.add.Text = "add";
+            this.add.UseVisualStyleBackColor = true;
+            this.add.Click += new System.EventHandler(this.add_Click);
+            // 
+            // remove
+            // 
+            this.remove.Location = new System.Drawing.Point(214, 306);
+            this.remove.Name = "remove";
+            this.remove.Size = new System.Drawing.Size(75, 23);
+            this.remove.TabIndex = 1;
+            this.remove.Text = "remove";
+            this.remove.UseVisualStyleBackColor = true;
+            this.remove.Click += new System.EventHandler(this.remove_Click);
+            // 
+            // display
+            // 
+            this.display.Location = new System.Drawing.Point(334, 306);
+            this.display.Name = "display";
+            this.display.Size = new System.Drawing.Size(75, 23);
+            this.display.TabIndex = 2;
+            this.display.Text = "display";
+            this.display.UseVisualStyleBackColor = true;
+            this.display.Click += new System.EventHandler(this.display_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(165, 96);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(393, 86);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 84);
+            this.listBox1.TabIndex = 4;
+            // 
+            // clear
+            // 
+            this.clear.Location = new System.Drawing.Point(463, 306);
+            this.clear.Name = "clear";
+            this.clear.Size = new System.Drawing.Size(75, 23);
+            this.clear.TabIndex = 5;
+            this.clear.Text = "clear";
+            this.clear.UseVisualStyleBackColor = true;
+            this.clear.Click += new System.EventHandler(this.clear_Click);
+            // 
+            // count
+            // 
+            this.count.Location = new System.Drawing.Point(580, 306);
+            this.count.Name = "count";
+            this.count.Size = new System.Drawing.Size(75, 23);
+            this.count.TabIndex = 6;
+            this.count.Text = "count";
+            this.count.UseVisualStyleBackColor = true;
+            this.count.Click += new System.EventHandler(this.count_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.OliveDrab;
+            this.panel1.Location = new System.Drawing.Point(1, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(33, 445);
+            this.panel1.TabIndex = 7;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.OliveDrab;
+            this.panel2.Location = new System.Drawing.Point(765, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(33, 445);
+            this.panel2.TabIndex = 8;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.OliveDrab;
+            this.panel3.Location = new System.Drawing.Point(24, 2);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(746, 30);
+            this.panel3.TabIndex = 9;
+            // 
+            // panel4
+            // 
+            this.panel4.BackColor = System.Drawing.Color.OliveDrab;
+            this.panel4.Location = new System.Drawing.Point(24, 418);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(746, 29);
+            this.panel4.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.count);
+            this.Controls.Add(this.clear);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.display);
+            this.Controls.Add(this.remove);
+            this.Controls.Add(this.add);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button add;
+        private System.Windows.Forms.Button remove;
+        private System.Windows.Forms.Button display;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button clear;
+        private System.Windows.Forms.Button count;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Panel panel4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace LinkedListExample
+{
+    public partial class Form1 : Form
+    {
+        private LinkedList<string> linkedList;
+        public Form1()
+        {
+            InitializeComponent();
+            linkedList = new LinkedList<string>();
+            System.Windows.Forms.Button addButton = new System.Windows.Forms.Button();
+            System.Windows.Forms.Button removeButton = new System.Windows.Forms.Button();
+            System.Windows.Forms.Button displayButton = new System.Windows.Forms.Button();
+            System.Windows.Forms.Button clearButton = new System.Windows.Forms.Button();
+            System.Windows.Forms.Button countButton = new System.Windows.Forms.Button();
+            System.Windows.Forms.TextBox textBox1 = new System.Windows.Forms.TextBox();
+            System.Windows.Forms.ListBox listBox1 = new System.Windows.Forms.ListBox();
+
+            // Настройки кнопок
+            addButton.Text = "Add";
+            addButton.Font = new Font("Arial", 10, FontStyle.Bold);
+            addButton.BackColor = Color.Green;
+            addButton.ForeColor = Color.White;
+
+            removeButton.Text = "Remove";
+            removeButton.Font = new Font("Arial", 10, FontStyle.Bold);
+            removeButton.BackColor = Color.Red;
+            removeButton.ForeColor = Color.White;
+
+            displayButton.Text = "Display";
+            displayButton.Font = new Font("Arial", 10, FontStyle.Bold);
+            displayButton.BackColor = Color.Blue;
+            displayButton.ForeColor = Color.White;
+
+            clearButton.Text = "Clear";
+            clearButton.Font = new Font("Arial", 10, FontStyle.Bold);
+            clearButton.BackColor = Color.Yellow;
+            clearButton.ForeColor = Color.Black;
+
+            countButton.Text = "Count";
+            countButton.Font = new Font("Arial", 10, FontStyle.Bold);
+            countButton.BackColor = Color.Orange;
+            countButton.ForeColor = Color.White;
+
+            // Настройки текстового поля
+            textBox1.Font = new Font("Arial", 10);
+
+            // Настройки ListBox
+            listBox1.Font = new Font("Arial", 10);
+
+            // Размещение элементов управления на форме
+            Controls.Add(addButton);
+            Controls.Add(removeButton);
+            Controls.Add(displayButton);
+            Controls.Add(clearButton);
+            Controls.Add(countButton);
+            Controls.Add(textBox1);
+            Controls.Add(listBox1);
+
+            // Дополнительные настройки формы
+            Text = "LinkedList Example";
+            Size = new Size(400, 300);
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            string inputText = textBox1.Text;
+
+            if (!string.IsNullOrEmpty(inputText))
+            {
+                linkedList.AddLast(inputText);
+                textBox1.Clear();
+                listBox1.Items.Add(inputText); // Add the entry to listBox1
+            }
+        }
+
+        private void remove_Click(object sender, EventArgs e)
+        {
+            if (linkedList.Count > 0)
+            {
+                linkedList.RemoveFirst();
+                listBox1.Items.RemoveAt(0); // Remove the first entry from listBox1
+            }
+        }
+
+        private void display_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            foreach (string item in linkedList)
+            {
+                listBox1.Items.Add(item);
+            }
+        }
+
+        private void clear_Click(object sender, EventArgs e)
+        {
+            linkedList.Clear();
+            listBox1.Items.Clear();
+        }
+
+        private void count_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Number of items in the linked list: " + linkedList.Count);
+            listBox1.SelectedIndex = -1; // Clear the selection in listBox1
+        }
+
+    }
+    public class Node<T>
+    {
+        public T Data { get; set; }
+        public Node<T> Next { get; set; }
+
+        public Node(T data)
+        {
+            Data = data;
+            Next = null;
+        }
+    }
+
+    public class LinkedList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public int Count { get; private set; }
+
+        public LinkedList()
+        {
+            head = null;
+            tail = null;
+            Count = 0;
+        }
+
+        public void AddLast(T data)
+        {
+            Node<T> newNode = new Node<T>(data);
+
+            if (head == null)
+            {
+                head = newNode;
+                tail = newNode;
+            }
+            else
+            {
+                tail.Next = newNode;
+                tail = newNode;
+            }
+
+            Count++;
+        }
+
+        public void RemoveFirst()
+        {
+            if (head == null)
+            {
+                throw new InvalidOperationException("LinkedList is empty.");
+            }
+
+            head = head.Next;
+
+            if (head == null)
+            {
+                tail = null;
+            }
+
+            Count--;
+        }
+
+        public void Clear()
+        {
+            head = null;
+            tail = null;
+            Count = 0;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            Node<T> current = head;
+
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+    }
+}
